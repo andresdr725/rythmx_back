fca08ea24348e91cb1a5fa33ad05a63c93470195
andres robles <andresinge07@gmail.com>
2025-12-09 16:52:59 -0500

    refactor(api): update search and album routers to use controller factory

--- app/api/routes/album_router.py
+++ app/api/routes/album_router.py
@@ -1,12 +1,13 @@
-from fastapi import APIRouter, Depends, Request
 from app.application.use_cases.albums.get_album_usecase import GetAlbumUseCase
-from core.contracts.usecase_contract import UseCaseArgs
 from app.api.config.factory.dependencies_factory import get_album_controller
+from core.contracts.usecase_contract import UseCaseArgs
+from core.models.response_model import ResponseHttp
+from app.domain.models.ytmusic.album import Album
+from fastapi import APIRouter, Depends, Request
 
 router = APIRouter(prefix="/api/album", tags=["Albums"])
 
-
-@router.get("/{album_id}")
+@router.get("/{album_id}", response_model=ResponseHttp[Album])
 async def get_album(album_id: str):
     # Obtener controlador del factory
     controller = get_album_controller()
--- app/api/routes/search_router.py
+++ app/api/routes/search_router.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter, Depends
-from app.application.dtos.inputs.search_input import SearchInput
 from app.api.config.factory.dependencies_factory import search_controller
+from core.models.response_model import ResponseHttp
+from app.application.dtos.inputs.search_input import SearchInput
+from app.domain.models.ytmusic.search import Search
 from fastapi.responses import JSONResponse
+from fastapi import APIRouter, Depends
 
 router = APIRouter(prefix="/api/search", tags=["Search"])
 
-@router.get("")
+@router.get("", response_model=ResponseHttp[Search])
 async def search(request: SearchInput = Depends()):
     controller = search_controller()
     response = await controller.execute(request)
